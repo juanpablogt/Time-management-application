d933070eb9db0772f9ddbc469d82791064c78a36
JPGonzalezT <juapabgonzalez@utp.edu.co>
2023-11-28 00:39:55 -0500

    learn strings whit pzthon

--- learnPY.py
+++ learnPY.py
@@ -6,3 +6,16 @@ print(var)
 
 name = "Adda lovelace"
 print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "juan"
+last_name ="gonzalez"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("hello, "+ var.title() +"!")
+
+message = "hello, "+ full_name.title() +"!"
+print(message)
