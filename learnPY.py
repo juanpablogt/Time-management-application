33d1300bad6f9ab70ea5b8d43c6e5a4e53bb8dbb
JPablo <juapabgonzalez@utp.edu.co>
2023-11-29 00:23:50 -0500

    jumps and whitespaces

--- learnPY.py
+++ learnPY.py
@@ -1,7 +1,6 @@
 var = "hallo welt"
 print(var)
 
-
 # String
 
 name = "Adda lovelace"
@@ -19,3 +18,19 @@ print("hello, "+ var.title() +"!")
 
 message = "hello, "+ full_name.title() +"!"
 print(message)
+
+# Whitespace
+print("Python")
+print("\tPython")
+print("lenguages:\nPython\nC\njavascript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+#whitespace
+
+favorite_language = ' python '  
+favorite_language.rstrip()
+print(favorite_language)
+favorite_language.lstrip()
+print(favorite_language)
+favorite_language.strip()
+print(favorite_language)
